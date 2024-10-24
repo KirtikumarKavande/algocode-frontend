54c942b88e587df97cd59c98d505f6ce3ac70d52
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-10-24 21:17:16 +0530

    feat:add navbar for information

--- src/components/headers/ProblemHeader.tsx
+++ src/components/headers/ProblemHeader.tsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React from "react";
 
 const ProblemHeader = () => {
   return (
-    <div className='w-full h-12 '>ProblemHeader</div>
-  )
-}
+    <div className="w-full h-16  box-border ">
+      <div className="navbar bg-base-100 border   border-b-2">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a>Homepage</a>
+              </li>
+              <li>
+                <a>Portfolio</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-center">
+          <a className="btn btn-ghost text-xl">AlgoCode</a>
+        </div>
+        <div className="navbar-end">
+      
+          <button className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                />
+              </svg>
+              <span className="badge badge-xs badge-primary indicator-item"></span>
+            </div>
+          </button>
 
-export default ProblemHeader
+          <button className="btn btn-ghost btn-circle">
+          <img
+          className="h-7"
+            alt="Tailwind CSS Navbar component"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+           </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProblemHeader;
