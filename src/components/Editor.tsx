a5ef971746308214151e569ea83969ff4d17157a
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-10-24 10:51:42 +0530

    feat: add auto-complete feature in ace editor for all lang

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,14 +1,30 @@
 import AceEditor from "react-ace";
+import { Editor as IAceEditor } from '@types/ace';
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/mode-python";
 import "ace-builds/src-noconflict/mode-java";
 import "ace-builds/src-noconflict/mode-c_cpp";
 import "ace-builds/src-noconflict/theme-monokai";
+import 'ace-builds/src-noconflict/ext-language_tools';  // This is necessary for enabling autocomplete
+import 'ace-builds/src-noconflict/ext-beautify'; 
+
 
 const Editor = ({ selectedLang }: { selectedLang: string }) => {
   function code(c: string) {
     console.log(c);
   }
+
+  
+
+  const onLoad = (editor:IAceEditor) => {
+    editor.setOptions({
+      enableBasicAutocompletion: true,  // Enable basic auto completion
+      enableLiveAutocompletion: true,   // Enable live autocompletion
+      enableSnippets: true,             // Enable snippets for autocompletion
+      showLineNumbers: true,
+      tabSize: 2,
+    });
+  };
   return (
     <div>
       {" "}
@@ -19,8 +35,13 @@ const Editor = ({ selectedLang }: { selectedLang: string }) => {
         onChange={code}
         name="UNIQUE_ID_OF_DIV"
         editorProps={{ $blockScrolling: true }}
-        height="635px"
-        fontSize={14}
+        // height={"100vh"}
+        fontSize={16}
+        enableBasicAutocompletion={true}
+        enableSnippets={true}
+        enableLiveAutocompletion={true}
+      
+        onLoad={onLoad}
       />
     </div>
   );
