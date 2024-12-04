81ce39ae4a7a3d83b48fc2d9c8550ab7b26f3e61
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-12-04 10:02:55 +0530

    style: css changes
    -selecting problem modal will close
    -logo added
    -link to problem admin service

--- src/components/ProblemList.tsx
+++ src/components/ProblemList.tsx
@@ -1,69 +1,84 @@
 import { useDispatch, useSelector } from "react-redux";
-import { RootState } from '../store/store';
+import { RootState } from "../store/store";
 import { getSelectedProblemId } from "../store/slices/problem.slice";
+import useGetSelectedProblem from "../hooks/useGetSelectedProblem";
 interface Problem {
   _id: string;
   title: string;
   difficulty: string;
 }
 
-const ProblemList = () => {
-    const problems:any  = useSelector((store: RootState)=>store.allProblems.problems)
-    const problemLinks=problems?.data
-    const dispatch=useDispatch()
-    
-      const difficultyChips:{ [key: string]: string } = {
-        Easy: 'bg-green-100 text-green-800',
-        Medium: 'bg-yellow-100 text-yellow-800',
-        Hard: 'bg-red-100 text-red-800'
-      };
+const ProblemList = ({ closeDrawer }: { closeDrawer: () => void }) => {
+  const problems: any = useSelector(
+    (store: RootState) => store.allProblems.problems
+  );
+  const problemLinks = problems?.data;
+  const dispatch = useDispatch();
+  const selectedProblemData = useGetSelectedProblem();
+  console.log("problems", selectedProblemData);
+  const difficultyChips: { [key: string]: string } = {
+    Easy: "bg-green-100 text-green-800",
+    Medium: "bg-yellow-100 text-yellow-800",
+    Hard: "bg-red-100 text-red-800",
+  };
 
-      function selectedProblem(id:string){
-        console.log("reached here")
-        dispatch(getSelectedProblemId(id)) 
-
-      }
+  function selectedProblem(id: string) {
+    console.log("reached here");
+    dispatch(getSelectedProblemId(id));
+    closeDrawer();
+  }
   return (
     <ul className="menu p-4 w-[500px] min-h-full bg-gray-800 text-white space-y-2">
-    {problemLinks && problemLinks.map((problem:Problem) => (
-      <li 
-        key={problem._id} 
-        className="group border-b border-gray-200 hover:bg-gray-800 pb-2 transition-colors"
-      >
-        <button
-        onClick={()=>selectedProblem(problem._id)}
-          className="flex justify-between items-center py-3 px-4 rounded-lg"
-        >
-          <div className="flex items-center space-x-4">
-            <span className="font-medium text-base">{problem.title.length > 40 ? `${problem.title.slice(0, 40)}...` : problem.title}</span>
-            <span 
-              className={`
+      {problemLinks &&
+        problemLinks.map((problem: Problem, index) => (
+          <li
+            key={problem._id}
+            className={`group border-b border-gray-200 hover:bg-gray-800 pb-2 transition-colors duration-300 ease-in-out ${
+              problem?._id === selectedProblemData?._id
+                ? "bg-gray-700 rounded-lg"
+                : !selectedProblemData && index === 0
+                ? "bg-gray-700 rounded-lg"
+                : ""
+            } `}
+          >
+            <button
+              onClick={() => selectedProblem(problem._id)}
+              className="flex justify-between items-center py-3 px-4 rounded-lg"
+            >
+              <div className="flex items-center space-x-4">
+                <span className="font-medium text-base">
+                  {problem.title.length > 40
+                    ? `${problem.title.slice(0, 40)}...`
+                    : problem.title}
+                </span>
+                <span
+                  className={`
                 px-2 py-0.5 rounded-full text-xs font-semibold
                 ${difficultyChips[problem.difficulty]}
               `}
-            >
-              {problem.difficulty}
-            </span>
-          </div>
-          <svg 
-            xmlns="http://www.w3.org/2000/svg" 
-            className="h-5 w-5 text-gray-400 opacity-0 group-hover:opacity-100 transition-opacity" 
-            fill="none" 
-            viewBox="0 0 24 24" 
-            stroke="currentColor"
-          >
-            <path 
-              strokeLinecap="round" 
-              strokeLinejoin="round" 
-              strokeWidth={2} 
-              d="M9 5l7 7-7 7" 
-            />
-          </svg>
-        </button>
-      </li>
-    ))}
-  </ul>
-  )
-}
+                >
+                  {problem.difficulty}
+                </span>
+              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 text-gray-400 opacity-0 group-hover:opacity-100 transition-opacity"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            </button>
+          </li>
+        ))}
+    </ul>
+  );
+};
 
-export default ProblemList
+export default ProblemList;
--- src/components/headers/ProblemHeader.tsx
+++ src/components/headers/ProblemHeader.tsx
@@ -1,14 +1,26 @@
+import { useRef } from "react";
 import ProblemList from "../ProblemList";
+import { Pencil } from "lucide-react";
 
 const ProblemHeader = () => {
- 
+  const drawerCheckboxRef = useRef<HTMLInputElement>(null);
+  const closeDrawer = () => {
+    if (drawerCheckboxRef.current) {
+      drawerCheckboxRef.current.checked = false;
+    }
+  };
   return (
     <div className="w-full h-16  box-border ">
       <div className="navbar bg-base-100 border   border-b-2">
         <div className="navbar-start">
           <div className="dropdown">
             <div className="drawer">
-              <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+              <input
+                ref={drawerCheckboxRef}
+                id="my-drawer"
+                type="checkbox"
+                className="drawer-toggle"
+              />
               <div className="drawer-content">
                 <label
                   htmlFor="my-drawer"
@@ -38,42 +50,53 @@ const ProblemHeader = () => {
                   aria-label="close sidebar"
                   className="drawer-overlay"
                 ></label>
-              <ProblemList/>
+                <ProblemList closeDrawer={closeDrawer} />
               </div>
             </div>
           </div>
         </div>
-        <div className="navbar-center">
-          <a className="btn btn-ghost text-xl">AlgoCode</a>
+        <div className="navbar-center flex items-center">
+          <img
+            className="h-9 w-12"
+            src="https://img.icons8.com/nolan/64/code--v2.png"
+            alt=""
+          />
+          <a className="btn btn-ghost text-2xl">AlgoCode</a>
         </div>
         <div className="navbar-end">
-          <button className="btn btn-ghost btn-circle">
-            <div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  className="h-7"
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.icons8.com/officel/80/administrator-male.png"
                 />
-              </svg>
-              <span className="badge badge-xs badge-primary indicator-item"></span>
+              </div>
             </div>
-          </button>
-
-          <button className="btn btn-ghost btn-circle">
-            <img
-              className="h-7"
-              alt="Tailwind CSS Navbar component"
-              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-            />
-          </button>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-48 p-2 shadow"
+            >
+              <li>
+                <div
+                  className="flex"
+                >
+                  <div>
+                    <Pencil size={20} />
+                  </div>
+                  <div>
+                    <a target="_blank" href="http://localhost:8080">Create Problem</a>
+                  </div>
+                </div>
+              </li>
+              <li></li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
